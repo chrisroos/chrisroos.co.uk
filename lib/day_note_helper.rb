61698f037df7bcbb3896ccede7aa6b38b33b5e4a
Chris Roos <chris@seagul.co.uk>
2010-10-26 11:09:24 +0100

    I don't want to have to think about how to summarise the post.

--- lib/day_note_helper.rb
+++ lib/day_note_helper.rb
@@ -1,11 +1,19 @@
-# require 'date'
 require 'active_support/inflector'
 
-def day_note_title(filename)
+def date_from_filename(filename)
   # filename will be of the form: 'yyyy mm dd day notes'
   date = filename[/(\d{4} \d{2} \d{2})/, 1]
-  date = Date.strptime(date, "%Y %m %d")
+  Date.strptime(date, "%Y %m %d")
+end
 
+def day_note_title(filename)
+  date            = date_from_filename(filename)
   ordinalised_day = ActiveSupport::Inflector.ordinalize(date.day)
   ['Day notes', date.strftime("%a #{ordinalised_day} %b %Y")].join(' - ')
+end
+
+def day_note_summary(filename)
+  date            = date_from_filename(filename)
+  ordinalised_day = ActiveSupport::Inflector.ordinalize(date.day)
+  ['Notes from', date.strftime("%A #{ordinalised_day} %B %Y")].join(' ')
 end
