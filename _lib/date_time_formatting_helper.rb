a1abbdc773470412a166d1968205a18c1664059a
Chris Roos <chris@seagul.co.uk>
2015-10-16 12:02:43 +0100

    Replace short_date helper
    The `date_to_xmlschema` liquid filter appears to give the same output as
    `ActiveSupport::TimeWithZone#xmlschema`, which is great.

--- _lib/date_time_formatting_helper.rb
+++ _lib/date_time_formatting_helper.rb
@@ -3,10 +3,6 @@ module DateTimeFormattingHelper
   def rfc822_datetime(page)
     page.created_at.rfc822
   end
-  def short_date(page)
-    date = page.created_at.strftime("%a, %d %b %y")
-    %%<abbr class="createdAt" title="#{page.created_at.xmlschema}">#{date}</abbr>%
-  end
 end
 
 Webby::Helpers.register(DateTimeFormattingHelper)
